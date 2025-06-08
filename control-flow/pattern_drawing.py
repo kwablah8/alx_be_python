2e2c00d6519c73d7cd84a930cbbf76874dd7ff0c
Kwablah <ernestkwablah19@gmail.com>
2025-06-08 15:00:02 +0000

    Add row variable and assign its value

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,8 @@
 pattern_size = int(input("Enter the size of the pattern: "))
+row = 0
 
-while pattern_size > 0:
+while pattern_size > row:
     for i in range(pattern_size):
         print("*", end="")
     print()
+    row += 1
