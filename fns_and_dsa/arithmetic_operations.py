4a00d2a1d595957b8062e1e28f4cbe36916f3759
Kwablah <ernestkwablah19@gmail.com>
2025-06-17 19:10:32 +0000

    adjust code to check for divide by zero scenario

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,5 +5,5 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    elif num1 or num2 == 0 and operation == "divide":
+    elif  operation == "divide" and num1 or num2 == 0:
         return num1 / num2
