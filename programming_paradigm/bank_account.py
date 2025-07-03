70ded988c37e472e56ff162d1c2d1ba19b1407bd
Kwablah <ernestkwablah19@gmail.com>
2025-07-03 17:58:10 +0000

    checks for correct output for withdrawals more than you have

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,8 +9,10 @@ class BankAccount:
     def withdraw(self, amount):
         if amount <= self.account_balance:
             self.account_balance -= amount
+            return True
         else:
-            print("Insufficient funds")
+            print("Insufficient funds.")
+            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
