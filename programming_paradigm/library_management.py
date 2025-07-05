3d48a3d2c0437b3dea2c2d6c0d1f8823764c4d9b
Kwablah <ernestkwablah19@gmail.com>
2025-07-05 22:55:01 +0000

    initial commit

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,54 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+     
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+    
+    def is_available(self):
+        return not self._is_checked_out
+
+
+
+class Library:
+    def __init__(self):
+        self.__books = []  # Private list of Book instances
+
+    def add_book(self, book):
+        self.__books.append(book)
+
+    def check_out_book(self, title):
+        for book in self.__books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                print(f"'{title}' has been checked out.")
+                return
+        print(f"'{title}' is not available for checkout.")
+
+    def return_book(self, title):
+        for book in self.__books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                print(f"'{title}' has been returned.")
+                return
+        print(f"'{title}' is not currently checked out.")
+
+    def list_available_books(self):
+        available = [book for book in self.__books if book.is_available()]
+        if not available:
+            print("No books available.")
+        else:
+            print("Available books:")
+            for book in available:
+                print(f"- {book.title} by {book.author}")
