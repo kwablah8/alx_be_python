276183dfb0b9ba35ba49a6c73161609e3f9005d9
Kwablah <ernestkwablah19@gmail.com>
2025-07-06 09:43:47 +0000

    mod method in library

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -23,13 +23,13 @@ class Book:
 
 class Library:
     def __init__(self):
-        self.__books = []  # Private list of Book instances
+        self._books = []  # Private list of Book instances
 
     def add_book(self, book):
-        self.__books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title and book.is_available():
                 book.check_out()
                 print(f"'{title}' has been checked out.")
@@ -37,7 +37,7 @@ class Library:
         print(f"'{title}' is not available for checkout.")
 
     def return_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title and not book.is_available():
                 book.return_book()
                 print(f"'{title}' has been returned.")
@@ -45,7 +45,7 @@ class Library:
         print(f"'{title}' is not currently checked out.")
 
     def list_available_books(self):
-        available = [book for book in self.__books if book.is_available()]
+        available = [book for book in self._books if book.is_available()]
         if not available:
             print("No books available.")
         else:
